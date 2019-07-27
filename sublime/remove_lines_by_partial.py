0bf71e76dd6d022fda33d42bc01ff023abb972ec
manavortex <manavortex@gmail.com>
2019-07-27 17:08:35 +0200

    Update remove_lines_by_partial.py

--- sublime/remove_lines_by_partial.py
+++ sublime/remove_lines_by_partial.py
@@ -3,21 +3,34 @@ import sys
 import sublime
 import sublime_plugin
 
+#{ "keys": ["super+shift+r"], "command": "remove_lines_by_partial" },
+#{ "keys": ["super+shift+ctrl+r"], "command": "remove_lines_without_partial" },
+
 class RemoveLinesByPartialCommand(sublime_plugin.TextCommand):
-  regex = ""
+
+  pluginText = "remove lines containing "
 
   def on_done(self, user_input):
-    sublime.status_message("removing lines containing " + user_input)
-    self.regex = "(.*({}).*)($[\r\n])?".format(user_input)
-    self.remove_lines_from_buffer()
-    self.regex = ""
-
-  def remove_lines_from_buffer(self):
-
-      region = sublime.Region(0, self.view.size())
-      for line in self.view.find_all(self.regex, 0):
-        self.view.replace(self.edit, line, "")
+    sublime.status_message(self.pluginText + user_input)
+    regex = "(.*({}).*)($[\r\n])?".format(user_input)
+    for line in self.view.find_all(regex, 0):
+      self.view.replace(self.edit, line, "")
 
   def run(self, edit):
     self.edit = edit
-    self.view.window().show_input_panel("remove lines containing", '', self.on_done, None, None)
+    self.view.window().show_input_panel(self.pluginText, '', self.on_done, None, None)
+
+
+class RemoveLinesWithoutPartialCommand(sublime_plugin.TextCommand):
+  pluginText = "remove lines _NOT_ containing "
+
+  def on_done(self, user_input):
+    sublime.status_message(self.pluginText + user_input)
+    regex = "^((?!|{}).)*$".format(user_input)
+    for line in self.view.find_all(regex, 0):
+      sublime.status_message(self.pluginText + user_input)
+      self.view.replace(self.edit, line, "")
+
+  def run(self, edit):
+    self.edit = edit
+    self.view.window().show_input_panel(self.pluginText, '', self.on_done, None, None)
