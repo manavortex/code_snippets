6836c61b89b403012f6bf8474e31915d4d31ef7b
manavortex <manavortex@gmail.com>
2019-07-27 16:38:45 +0200

    Update remove_lines_by_partial.py

--- sublime/remove_lines_by_partial.py
+++ sublime/remove_lines_by_partial.py
@@ -10,21 +10,13 @@ class RemoveLinesByPartialCommand(sublime_plugin.TextCommand):
     sublime.status_message("removing lines containing " + user_input)
     self.regex = "(.*({}).*)($[\r\n])?".format(user_input)
     self.remove_lines_from_buffer()
+    self.regex = ""
 
   def remove_lines_from_buffer(self):
 
       region = sublime.Region(0, self.view.size())
       for line in self.view.find_all(self.regex, 0):
         self.view.replace(self.edit, line, "")
-        line = self.view.find(self.regex, 0)
-      self.regex = ""
-
-  def match_line(text, case_sensitive=True, invert_search=False):
-    flags = 0
-    if not case_sensitive:
-        flags |= re.IGNORECASE
-    match = re.search(self.regex, text, flags)
-    return bool(match) ^ invert_search
 
   def run(self, edit):
     self.edit = edit
