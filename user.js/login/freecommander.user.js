10f933d65c208d4ce0cd45426fcfdacc22a9d529
manavortex <manavortex@gmail.com>
2019-10-09 09:08:53 +0200

    Update freecommander.user.js

--- user.js/login/freecommander.user.js
+++ user.js/login/freecommander.user.js
@@ -1,31 +1,31 @@
 // ==UserScript==
-// @name         FreeCommander: AutoLogin
-// @namespace    http://tampermonkey.net/
+// @name         AutoLogin: FreeCommander
+// @namespace    https://freecommander.com/
 // @version      0.1
 // @description  FreeCommander
-// @author       You
+// @author       manavortex
 // @match        https://freecommander.com/donors/*
 // @grant        none
 // ==/UserScript==
 
 var $ = window.jQuery;
 (function() {
-    setTimeout(function() {
     'use strict';
-    var passwordField = document.getElementById('amember-pass');
-    var error = document.querySelector('.am-errors am-login-errors');
-    if (null !== passwordField && passwordField.textLength > 0 && null === error ) {
-        document.querySelector("input[type='submit']").click();
-        return;
-    }
-    var linkToDownloads = document.querySelector('a[href*="/donors/page/downloads"]');
-    if (null !== linkToDownloads) {
-        linkToDownloads.click();
-        return;
-    }
-    var downloadLink = $('a:contains("FreeCommanderXE-64-donor_setup")');
-    if (null !== downloadLink && downloadLink.length > 0) {
-        downloadLink[0].click();
-    }
+    setTimeout(function() {
+        var passwordField = document.getElementById('amember-pass');
+        var error = document.querySelector('.am-errors am-login-errors');
+        if (null !== passwordField && passwordField.textLength > 0 && null === error ) {
+            document.querySelector("input[type='submit']").click();
+            return;
+        }
+        var linkToDownloads = document.querySelector('a[href*="/donors/page/downloads"]');
+        if (null !== linkToDownloads) {
+            linkToDownloads.click();
+            return;
+        }
+        var downloadLink = $('a:contains("FreeCommanderXE-64-donor_setup")');
+        if (null !== downloadLink && downloadLink.length > 0) {
+            downloadLink[0].click();
+        }
     }, 1500);
 })();
