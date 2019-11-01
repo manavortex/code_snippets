d9ae548922cf20aafec56632fb47e2731b7bbf90
manavortex <manavortex@gmail.com>
2019-11-01 18:04:32 +0100

    Update remove_lines_by_partial.py

--- sublime/remove_lines_by_partial.py
+++ sublime/remove_lines_by_partial.py
@@ -1,24 +1,17 @@
-import re
-import sys
 import sublime
 import sublime_plugin
 
-#{ "keys": ["super+shift+r"], "command": "remove_lines_by_partial" },
-#{ "keys": ["super+shift+ctrl+r"], "command": "remove_lines_without_partial" },
 
-class ReplaceLinesCommand(sublime_plugin.TextCommand):
+class ReplaceLinesWithPartialCommand(sublime_plugin.TextCommand):
+  pluginText = "remove lines containing "
+
   # Check all links in view
   def check_lines(self, edit, regex_arg):
-
     
     v = self.view
 
     # Find all lines in view
-    regions = [s for s in v.sel() if not s.empty()]
-
-  	# if there's no non-empty selection, filter the whole document
-    if len(regions) == 0:
-    	regions = [ sublime.Region(0, v.size()) ]
+    regions = [ sublime.Region(0, v.size()) ]
 
     for region in reversed(regions):
       lines = v.split_by_newlines(region)
@@ -27,7 +20,30 @@ class ReplaceLinesCommand(sublime_plugin.TextCommand):
         if regex_arg in v.substr(line):
           v.erase(edit, v.full_line(line))
 
-  def run(self, edit, regex_arg):
-    if self.view.is_read_only() or self.view.size () == 0:
-      return
-    self.check_lines (edit, regex_arg)
+  def run(self, edit, *regex_arg):
+    if self.view.is_read_only() or self.view.size () == 0 : return
+    self.check_lines(edit, self.view.substr(self.view.sel()[0]))
+
+class ReplaceLinesWithoutPartialCommand(sublime_plugin.TextCommand):
+  pluginText = "remove lines NOT containing "
+
+  # Check all links in view
+  def check_lines(self, edit, regex_arg):
+    
+    v = self.view
+
+    # if there's no non-empty selection, filter the whole document
+
+    regions = [ sublime.Region(0, v.size()) ]
+
+    for region in reversed(regions):
+      lines = v.split_by_newlines(region)
+
+      for line in reversed(lines):
+        if regex_arg not in v.substr(line):
+          v.erase(edit, v.full_line(line))
+
+  
+  def run(self, edit):
+    if self.view.is_read_only() or self.view.size () == 0 : return
+    self.check_lines(edit, self.view.substr(self.view.sel()[0]))
