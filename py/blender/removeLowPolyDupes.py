cb700ee18c1c7bc38ba7cff33240f6a916c4773f
manavortex <manavortex@gmail.com>
2020-02-10 20:27:16 +0100

    Create removeLowPolyDupes.py

---
+++ py/blender/removeLowPolyDupes.py
@@ -0,0 +1,40 @@
+import bpy
+scene = bpy.context.scene
+
+def delete_object(name):
+    bpy.ops.object.select_all(action='DESELECT')
+    # https://wiki.blender.org/wiki/Reference/Release_Notes/2.80/Python_API/Scene_and_Object_API
+    bpy.data.objects[name].select_set(True) # Blender 2.8x
+    bpy.ops.object.delete() 
+
+
+def clean_objects(skeleton):
+    keepMesh = "none"
+    meshName = ""
+    polyCount = 0
+    meshMap = {}
+    for child in skeleton.children:
+        if not  "MESH" == child.type: 
+            continue; 
+        dotIndex = child.name.find(".")
+        
+        tmpName = child.name[:dotIndex] if dotIndex > 0 else child.name
+
+        if not tmpName in meshMap.keys(): 
+            meshMap[tmpName] = {}
+        meshMap[tmpName][len(child.data.polygons)] = child.name
+    for meshName, meshData in meshMap.items():
+        first = True
+        for key, value in sorted(meshData.items(), reverse = True):
+            if first: 
+                first = False
+                continue
+            delete_object(value)
+
+
+root_obs = (o for o in scene.objects if not o.parent)
+
+
+for o in root_obs:
+    if "Skeleton" in o.name: 
+        clean_objects(o)
