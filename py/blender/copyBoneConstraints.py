f603c07d009efd584680b3597216d15ff4962429
manavortex <manavortex@gmail.com>
2020-02-15 16:33:50 +0100

    Update copyBoneConstraints.py

--- py/blender/copyBoneConstraints.py
+++ py/blender/copyBoneConstraints.py
@@ -2,23 +2,38 @@ import bpy
 
 # based on https://blender.stackexchange.com/a/40246
 
-copyFrom = "Left"
-copyTo = "Right"
+bones = bpy.context.selected_pose_bones if bpy.context.selected_pose_bones else bpy.context.object.pose.bones
+L = "Left"
+R = "Right"
 
-pBones = [ b for b in bpy.context.object.pose.bones if copyFrom in b.name and 0 < len(b.constraints) ]
+numLeft = sum(L in bone.name for bone in bones)
+numRight = sum(R in bone.name for bone in bones)
 
+copyFrom = L if numLeft > numRight else R
+copyTo = R if numLeft > numRight else L
+
+pBones = [ b for b in bones if copyFrom in b.name ]
 for b in pBones:
-    otherBone = bpy.context.object.pose.bones[  b.name.replace( copyFrom, copyTo ) ]
-    for c in b.constraints:         
+    for c in b.constraints:
+        
+        otherBone = bpy.context.object.pose.bones[
+            b.name.replace( copyFrom, copyTo ) 
+        ]
+        
+        otherConstraints = [ b for b in  otherBone.constraints if c.type == b.type ]
+        for otherC in otherConstraints :
+            otherBone.constraints.remove( otherC ) # Remove constraint
+            
+        nc = otherBone.constraints.new( c.type )
 
-        nc = otherBone.constraints.new( c.type ) 
-        for prop in dir( c ):   
+        for prop in dir( c ):
             # This is fairly ugly and dirty, but quick and does the trick...
             try:
                 constrProp = getattr( c, prop )
-                if type( constrProp ) == type(str()) and (copyFrom in constrProp or copyTo in constrProp):
-                    oppositeVal = constrProp.replace(copyFrom, copyTo) if copyFrom in constrProp else constrProp.replace(copyTo, copyFrom)                     
-                    setattr( nc, prop, oppositeVal ) 
+                if type( constrProp) == type(str()) and (copyFrom in constrProp or copyTo in constProp): 
+                    # Replace string property values from L to R and vice versa
+                    oppositeVal = constrProp.replace(copyFrom, copyTo) if copyFrom in constrProp else constrProp.replace(copyTo, copyFrom)
+                    setattr( nc, prop, oppositeVal )
                 elif 'max_' in prop:
                     setattr( nc, prop, getattr( c, prop.replace( 'max', 'min' ) ) )
                 elif 'min_' in prop:
