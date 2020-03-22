9c4119c2632fd5ae3cea6281152c1234364ab9b6
manavortex <manavortex@gmail.com>
2020-03-22 15:58:31 +0100

    Update removeLowPolyDupes.py

--- py/blender/removeLowPolyDupes.py
+++ py/blender/removeLowPolyDupes.py
@@ -5,39 +5,43 @@ def delete_object(name):
     bpy.ops.object.select_all(action='DESELECT')
     # https://wiki.blender.org/wiki/Reference/Release_Notes/2.80/Python_API/Scene_and_Object_API
     bpy.data.objects[name].select_set(True) # Blender 2.8x
-    bpy.ops.object.delete() 
+    bpy.ops.object.delete()
 
+meshMap = {}
+deleteMap = {}
 
 def clean_objects(skeleton):
-    keepMesh = "none"
-    meshName = ""
-    polyCount = 0
-    meshMap = {}
-    for child in skeleton.children:
-        if not  "MESH" == child.type: 
-            continue; 
-        dotIndex = child.name.find(".")
-        
-        tmpName = child.name[:dotIndex] if dotIndex > 0 else child.name
 
-        if not tmpName in meshMap.keys(): 
+    for child in skeleton.children:
+        if not  "MESH" == child.type:
+            continue;
+        dotIndex = child.name.find(".")
+        tmpName = skeleton.name + "_" + (child.name[:dotIndex] if dotIndex > 0 else child.name)
+
+        if not tmpName in meshMap.keys():
             meshMap[tmpName] = {}
         meshMap[tmpName][len(child.data.polygons)] = child.name
-    for meshName, meshData in meshMap.items():
-        first = True
-        for key, value in sorted(meshData.items(), reverse = True):
-            if first: 
-                first = False
-                continue
-            delete_object(value)
+
 
 
 root_obs = (o for o in scene.objects if not o.parent)
 
-
 for o in root_obs:
-    print(o.type)
-    if "Skeleton" in o.name: 
+    if "Skeleton" in o.name:
         clean_objects(o)
     if "EMPTY" == o.type and len(o.children) == 0:
-        delete_object(o.name)
+        deleteMap[o.name] = o.name
+
+print(meshMap.items())
+
+for meshName, meshData in meshMap.items():
+        first = True
+        for key, value in sorted(meshData.items(), reverse = True):
+            if first:
+                print("Keeping " + value)
+                first = False
+                continue
+            deleteMap[key] = value
+
+for key, value in deleteMap.items():
+    delete_object(value)
