4994998f14524131d1c200b41e0ca17768276c04
manavortex <manavortex@gmail.com>
2020-03-22 15:45:03 +0100

    Update removeLowPolyDupes.py

--- py/blender/removeLowPolyDupes.py
+++ py/blender/removeLowPolyDupes.py
@@ -1,11 +1,7 @@
 import bpy
 scene = bpy.context.scene
 
-wasCleaned = False
-meshMap = {}
-
 def delete_object(name):
-    print("delete_object(" + name + ") called")
     bpy.ops.object.select_all(action='DESELECT')
     # https://wiki.blender.org/wiki/Reference/Release_Notes/2.80/Python_API/Scene_and_Object_API
     bpy.data.objects[name].select_set(True) # Blender 2.8x
@@ -13,6 +9,10 @@ def delete_object(name):
 
 
 def clean_objects(skeleton):
+    keepMesh = "none"
+    meshName = ""
+    polyCount = 0
+    meshMap = {}
     for child in skeleton.children:
         if not  "MESH" == child.type: 
             continue; 
@@ -23,20 +23,21 @@ def clean_objects(skeleton):
         if not tmpName in meshMap.keys(): 
             meshMap[tmpName] = {}
         meshMap[tmpName][len(child.data.polygons)] = child.name
-    
-
-
-root_obs = (o for o in scene.objects if not o.parent)
-for o in root_obs: 
-    if "EMPTY" == o.type and len(o.children) == 0:
-        delete_object(o.name)
-    elif "Skeleton" in o.name: 
-        clean_objects(o)
-
-for meshName, meshData in meshMap.items():
+    for meshName, meshData in meshMap.items():
         first = True
         for key, value in sorted(meshData.items(), reverse = True):
             if first: 
                 first = False
                 continue
             delete_object(value)
+
+
+root_obs = (o for o in scene.objects if not o.parent)
+
+
+for o in root_obs:
+    print(o.type)
+    if "Skeleton" in o.name: 
+        clean_objects(o)
+    if "EMPTY" == o.type and len(o.children) == 0:
+        delete_object(o.name)
