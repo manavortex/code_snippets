152ad347d0aa00201a410145410355a3efa59aec
manavortex <manavortex@gmail.com>
2020-12-05 16:57:47 +0100

    Create replace_notepad_nplusplus.cmd

---
+++ batch/replace_notepad_nplusplus.cmd
@@ -0,0 +1,2 @@
+REM run from command line as admin
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v "Debugger" /t REG_SZ /d "\"%ProgramFiles(x86)%\Notepad++\notepad++.exe\" -notepadStyleCmdline -z" /f
