02d9a4b30ec8cb39a17f69b6183ab120d5c7d34b
manavortex <manavortex@gmail.com>
2022-10-27 15:09:47 +0200

    Update selected_vertices_print_groups.py

--- py/blender/selected_vertices_print_groups.py
+++ py/blender/selected_vertices_print_groups.py
@@ -1,23 +1,31 @@
 import bpy
+import bmesh
 
 
-obj=bpy.context.active_object
-selected_verts = list(filter(lambda v: v.select, obj.data.vertices))
+obj=bpy.context.object
+bm=bmesh.from_edit_mesh(obj.data)
 
-print("\n\n=============== groups of currently selected vertices ===================")
 
-for group in obj.vertex_groups: 
-    group_name = group.name
-    group_name_printed = False
+if not obj.mode == 'EDIT':    
+    print("Object is not in edit mode.")
+else:
+    obj=bpy.context.active_object
+    selected_verts = list(filter(lambda v: v.select, bm.verts))
 
-    for vert in selected_verts:
-        i=vert.index
-        try:
-            weight = obj.vertex_groups[group_name].weight(i)
-            #if we get past this line, then the vertex is present in the group
-            if (weight > 0) and not group_name_printed: 
-                group_name_printed = True
-                print(group.name)
-        except RuntimeError:
-           # vertex is not in the group
-           pass
+    print("\n\n=============== groups of currently selected vertices ===================")
+
+    for group in obj.vertex_groups:
+        group_name = group.name
+        group_name_printed = False
+
+        for vert in selected_verts:
+            i=vert.index
+            try:
+                weight = obj.vertex_groups[group_name].weight(i)
+                #if we get past this line, then the vertex is present in the group
+                if (weight > 0) and not group_name_printed: 
+                    group_name_printed = True
+                    print(group.name)
+            except RuntimeError:
+               # vertex is not in the group
+               pass
