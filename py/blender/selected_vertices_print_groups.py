697a888741a3af5a290e863263248f64a0ece946
manavortex <manavortex@gmail.com>
2022-10-27 15:01:07 +0200

    Create selected_vertices_print_groups.py

---
+++ py/blender/selected_vertices_print_groups.py
@@ -0,0 +1,23 @@
+import bpy
+
+
+obj=bpy.context.active_object
+selected_verts = list(filter(lambda v: v.select, obj.data.vertices))
+
+print("\n\n=============== groups of currently selected vertices ===================")
+
+for group in obj.vertex_groups: 
+    group_name = group.name
+    group_name_printed = False
+
+    for vert in selected_verts:
+        i=vert.index
+        try:
+            weight = obj.vertex_groups[group_name].weight(i)
+            #if we get past this line, then the vertex is present in the group
+            if (weight > 0) and not group_name_printed: 
+                group_name_printed = True
+                print(group.name)
+        except RuntimeError:
+           # vertex is not in the group
+           pass
