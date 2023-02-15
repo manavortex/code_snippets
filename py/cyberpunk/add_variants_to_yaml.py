b6356d47ba9a7e90988700dac6d845ee19bac0e9
manavortex <manavortex@gmail.com>
2023-02-15 21:04:05 +0100

    Update add_variants_to_yaml.py

--- py/cyberpunk/add_variants_to_yaml.py
+++ py/cyberpunk/add_variants_to_yaml.py
@@ -3,18 +3,35 @@ import os
 import collections
 import yaml
 import re
+import shutil
   
-variants = ['black_gold', 'black_leather', 'black_red', 'black_silver', 'carbon_fiber', 'glass_fiber', 'glitch_latex', 'hex_black', 'hex_glass', 'hex_glitch', 'hex_gold_black', 'hex_redblack', 'hex_silver_black', 'hex_white_black', 'hex_white_gold', 'hex_white_matteblack', 'plain_glass', 'white_black', 'white_rubber']
+variants = ['black_gold', 'black_silver']
 
 # Folder Path
-path = os.path.dirname(__file__)
+path = "E:\\path\\to\\your.yaml"
 
-def replace_in_dict(dict, source, target, slotNumber): 
+replaceme = "VARIANT"
+################################################### DO NOT EDIT BELOW THIS LINE ########################################################
+
+
+if not os.path.isfile("{}.orig".format(path)):
+    shutil.copy(path, "{}.orig".format(path))
+else:
+    os.remove(path) 
+    shutil.copy("{}.orig".format(path), path)   
+
+
+def append_constructor(loader: yaml.SafeLoader, node: yaml.nodes.ScalarNode) -> str:
+  return f"!append {loader.construct_scalar(node)} append!"
+
+yaml.SafeLoader.add_constructor("!append", append_constructor)
+
+def replace_in_dict(dict, source, target, currentSlotIdx): 
     ret = dict.copy()
     for key in ret.keys():
         value = ret[key]
         if isinstance(value, collections.abc.Mapping):
-            ret[key] = replace_in_dict(value, source, target, slotNumber)
+            ret[key] = replace_in_dict(value, source, target, currentSlotIdx)
         elif isinstance(value, str):
             ret[key] = re.sub(source, target, value)
         elif isinstance(value, list):
@@ -25,38 +42,58 @@ def replace_in_dict(dict, source, target, slotNumber):
         else:
             print(type(value))
         if (key == "atlasPartName"):
-            ret[key] = re.sub("00", str(slotNumber).zfill(2), value)                
+            ret[key] = re.sub("\d\d", str(currentSlotIdx).zfill(2), value)
 
     return ret
 
 
-# iterate through all file
-for file in os.listdir():
-    # Check whether file is in text format or not
-    if file.endswith(".yaml"):
-        file_path = f"{path}\{file}"    
+# Read YAML file
+with open(path, 'r') as stream:
+    text_content = yaml.safe_load(stream)
+    items = text_content.copy().items()
 
-        key = ""
-        value = None
+    highestSlotIndex = 0
+    for _key, _value in items:
+        # get highest atlas slot
+        if not ("icon" in _value and "atlasPartName" in _value["icon"]):
+                continue
+        atlasNameSlot = _value["icon"]["atlasPartName"]
+        
+        print("atlasNameSlot is " + str(atlasNameSlot))
+        if atlasNameSlot is not None:
+            slotNo = int(''.join(filter(str.isdigit, atlasNameSlot)))
+            slotNo = 0 if slotNo is None else int(slotNo)
+            print("slotNo is " + str(slotNo))
+            if slotNo > highestSlotIndex:
+                highestSlotIndex = slotNo
 
-        # Read YAML file
-        with open(file_path, 'r') as stream:
-            text_content = yaml.safe_load(stream)
+    newVariants = {}
 
-            for _key, _value in text_content.items():
-                if "VARIANT" in _key:
-                    key = _key
-                    value = _value
+    for _key, _value in items:
+        if not "VARIANT" in _key: 
+            continue; 
 
-            idx = 1
-            for appearanceName in variants: 
-                _key = re.sub("VARIANT", appearanceName, key)
-                _value = replace_in_dict(value, "VARIANT", appearanceName, idx)
-                idx += 1
+        for appearanceName in variants: 
+            key = _key.replace(replaceme, appearanceName)
+            value = replace_in_dict(_value, replaceme, appearanceName, highestSlotIndex)
+            highestSlotIndex += 1
 
-                text_content[_key] = _value
+            text_content[key] = value
 
-            del text_content[key]
 
-            with open("_output.yml", "w") as yaml_file:
-               yaml.dump(text_content, yaml_file)
+    with open("_output.yml", "w") as yaml_file:                    
+        yaml.dump(text_content, yaml_file)
+               
+
+# tidy up borked yaml tags. PYTHON WHY
+with open("_output.yml", 'r') as file :
+    filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("- '!append", '  - !append')
+    filedata = filedata.replace(" append!'", '')
+
+    # Write the file out again
+    with open('file.yml', 'w') as file:
+        file.write(filedata)
+            
