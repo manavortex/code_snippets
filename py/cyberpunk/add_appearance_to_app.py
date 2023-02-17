4ccac0be7cc47241e3b217523834b712e2b4444c
manavortex <manavortex@gmail.com>
2023-02-17 22:03:22 +0100

    Update add_appearance_to_app.py

--- py/cyberpunk/add_appearance_to_app.py
+++ py/cyberpunk/add_appearance_to_app.py
@@ -4,56 +4,53 @@ import re
 import copy
 import shutil
 
-path = "e:\\path_to\\your.app.json"
+path = "E:\\path\\to\\your.app.json"
 
 appearanceNames=["white", "black", "red", "pink", "navy", "green"]
 
-replaceme="VARIANT"
+# appearances to duplicate
+baselist=['ff_shirt','ff_pants','ff_belt']
 
+# components to ignore
+blacklist=['belt_buckle_component']
 
 if not os.path.isfile("{}.orig".format(path)):
     shutil.copy(path, "{}.orig".format(path))
-else:
-    os.remove(path) 
-    shutil.copy("{}.orig".format(path), path)
+#else:
+#    os.remove(path) 
+ #   shutil.copy("{}.orig".format(path), path)
 
 with open(path,'r') as f: 
     j=json.load(f)
-   
 
 t=j['Data']['RootChunk']
 appearances=t['appearances']
+
+existing_apps=[]
+for __app in appearances:
+    existing_apps.append(__app['Data']['name'])
+
+
 i = 0
-for _app in appearances:
-    if (not replaceme in _app['Data']['name']):
-        _app['HandleId'] = str(i)
-        _app['Data']['compiledData']["BufferId"] = str(i)
-        i += 1
-        continue
-    original =  copy.deepcopy(_app)
-    for appearanceName in appearanceNames:
-        app =  copy.deepcopy(original)
-        app['HandleId'] = str(i)
-        app['Data']['compiledData']["BufferId"] = str(i)
+for appearanceName in appearanceNames:
+    for _app in appearances:    
+        if (_app['Data']['name'] in baselist):
+            original =  copy.deepcopy(_app)        
+            newname=_app['Data']['name']+'_'+appearanceName
+            if newname not in existing_apps:
+                print(appearanceName)
+                app =  copy.deepcopy(original)
+                app['Data']['name'] =  newname
+                
+                for i,override in enumerate(app['Data']['partsOverrides']):
+                    
+                    for compOverride in override['componentsOverrides']:                
+                        if compOverride['meshAppearance'] not in blacklist: 
+                            compOverride['meshAppearance'] = appearanceName
 
-        app['Data']['name'] = re.sub(replaceme, appearanceName, app['Data']['name'])
 
-        overrides = []
-        for override in app['Data']['partsOverrides']:
-            compOverrides = []
-            for compOverride in override['componentsOverrides']:                
-                if not replaceme in compOverride['meshAppearance']: 
-                    compOverride['meshAppearance'] = appearanceName
-                else: 
-                    compOverride['meshAppearance'] = compOverride['meshAppearance'].replace(replaceme, appearanceName)
-                compOverrides.append(compOverride)
-            override['componentsOverrides'] = compOverrides
-            overrides.append(override)
-
-        app['Data']['partsOverrides'] = overrides
-
-        t['appearances'].append(app)
-        i += 1
+                t['appearances'].append(app)
+                i += 1
 
 
 
@@ -75,7 +72,8 @@ for x,line in enumerate(lines):
             eol=-2
         lines[x]=line[:line.find('"BufferId": "')+len('"BufferId": "')]+str(j)+line[eol:]
         j+=1
-
+    if '"components": null,' in line:
+        lines[x]=line.replace('"components": null,', '"components": [],')
 
 with open(path, 'w') as outfile:
     outfile.writelines(lines)
