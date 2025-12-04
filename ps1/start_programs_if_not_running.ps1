1c80424feff40e476eb852c5bad00f70c817f990
manavortex <manavortex@gmail.com>
2025-12-04 10:36:30 +0100

    Enhance script to launch Outlook with calendar view
    Added support for starting Outlook with calendar view if not running.

--- ps1/start_programs_if_not_running.ps1
+++ ps1/start_programs_if_not_running.ps1
@@ -7,7 +7,12 @@
 # value: full path to the file you want to run if the executable isn't running
 $applications = @{
     "KeePass" = "C:\Program Files (x86)\KeePass2x\KeePass.exe";
-    "AutoHotkeyV2" = "$env:userprofile\Dokumente\AutoHotkey.ahk";    
+    "AutoHotkeyV2" = "$env:userprofile\Dokumente\AutoHotkey.ahk";
+	"OUTLOOK.exe" = "OUTLOOK.exe";
+}
+
+$applications_args = @{
+  "OUTLOOK.exe" = "/select Outlook:Calendar";
 }
 
 # comment back in to wait 30 seconds (e.g. if you run this after boot)
@@ -20,13 +25,25 @@ $applications = @{
 foreach ($appName in $applications.Keys) {
   Write-Host "`nChecking for $appName..."
 
-  if (Get-Process -Name $appName.Replace(".exe", "") -ErrorAction SilentlyContinue)) {
+  if (Get-Process -Name $appName.Replace(".exe", "") -ErrorAction SilentlyContinue) {
 		continue		
   }
 	
-  $filePath = $applications[$appName]
-	
+  $filePath = $applications[$appName]	
   Write-Host "Not running. Starting $filePath..."
-  Start-Process -FilePath $filePath
-  Start-Sleep -Seconds 1
+
+  # get arguments from second list
+  $application_args = ""    
+  if ($applications_args.ContainsKey($appName)) {    
+    $application_args = $applications_args[$appName]    
+  }
+
+  # start new powershell instance via cmd, so that we don't have windows lurking around forever
+  cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Start-Process -FilePath $filePath" -ArgumentList $application_args 
+
+  # short delay for no reason (I like watching the text move)
+  Start-Sleep -Seconds 0.5
 }
+
+# short delay for no reason (I like watching the text move)
+Start-Sleep -Seconds 5
